335d4bbebe322b8d9e6a5c7a6284891b9984f1ec
rdharun <dharunr@testvagrant.com>
2024-02-27 21:09:06 +0530

    disable hooks for reporting

--- test/specs/login/login.test.js
+++ test/specs/login/login.test.js
@@ -1,50 +1,50 @@
-    const { expect } = require('chai');
-    const OtpPage = require('../../pageobjects/otpPage');
-    const HomePage = require('../../pageobjects/homePage');
-    const ProfilePage = require('../../pageobjects/profilePage');
-    const LoginPageUtil = require('../../commonFunctions/loginPageUtil');
-    const jsonData = require("../../../resources/credentials.json")
-    const Logger = require('../../../utilities/customLogger/loggerHelper');
+const { expect } = require('chai');
+const OtpPage = require('../../pageobjects/otpPage');
+const HomePage = require('../../pageobjects/homePage');
+const ProfilePage = require('../../pageobjects/profilePage');
+const LoginPageUtil = require('../../commonFunctions/loginPageUtil');
+const jsonData = require("../../../resources/credentials.json")
+const Logger = require('../../../utilities/customLogger/loggerHelper');
 
 
-    describe('My Login application', () => {
+describe('My Login application', () => {
 
 
-        let homePage;
-        let profilePage;
-        let otpPage;
-        let loginPageUtil;
-        let validCredentials;
-        let LOGGER;
+    let homePage;
+    let profilePage;
+    let otpPage;
+    let loginPageUtil;
+    let validCredentials;
+    let LOGGER;
 
-        const specName = 'Test_login_scenarios';
-        before(async () => {
-            homePage = new HomePage();
-            profilePage = new ProfilePage();
-            otpPage = new OtpPage();
-            loginPageUtil = new LoginPageUtil();
-            validCredentials = jsonData.credentialsSets.validCredentials;
-            LOGGER = new Logger(specName);
-        });
+    const specName = 'Test_login_scenarios';
+    before(async () => {
+        homePage = new HomePage();
+        profilePage = new ProfilePage();
+        otpPage = new OtpPage();
+        loginPageUtil = new LoginPageUtil();
+        validCredentials = jsonData.credentialsSets.validCredentials;
+        LOGGER = new Logger(specName);
+    });
 
-        afterEach(async () => {
-            await loginPageUtil.logout();
-        })
+    afterEach(async () => {
+        await loginPageUtil.logout();
+    })
 
 
-        it('should login with valid credentials', async () => {
-            try {
-                LOGGER.info('starting the test')
-                await loginPageUtil.login(validCredentials.username, validCredentials.password);
-                await otpPage.enterOtp(validCredentials.otp);
-                const userNameText = await (await homePage.getUserNameEle()).getText();
-                expect(userNameText).to.equal('Jack Sparrow');
-                LOGGER.info('ending the test');
-            } catch (error) {
-                LOGGER.error('An error occurred during the test:', error);
-                throw error;
-            }
-        
-        })
+    it('should login with valid credentials', async () => {
+        try {
+            LOGGER.info('starting the test')
+            await loginPageUtil.login(validCredentials.username, validCredentials.password);
+            await otpPage.enterOtp(validCredentials.otp);
+            const userNameText = await (await homePage.getUserNameEle()).getText();
+            expect(userNameText).to.equal('Jack Sparrow');
+            LOGGER.info('ending the test');
+        } catch (error) {
+            LOGGER.error('An error occurred during the test:', error);
+            throw error;
+        }
 
     })
+
+})
